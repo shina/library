52e4516769a6bc9b8baebe2559d607dff1c6b141
Leonardo Shinagawa <leonardo@ebussola.com>
2021-03-16 19:22:31 +0100

    fix: typescript complaint on missing parameter

--- pipe.js
+++ pipe.js
@@ -2,4 +2,4 @@
  * Eric Elliott's pipe function
  * @see https://medium.com/javascript-scene/composing-software-an-introduction-27b72500d6ea
  */
-export const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+export const pipe = (...fns) => (x = undefined) => fns.reduce((v, f) => f(v), x);
