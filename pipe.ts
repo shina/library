4ce33c53b80903abf69b1bf9f271d7e85570e30e
Leonardo Shinagawa <leonardo@ebussola.com>
2021-03-16 19:31:10 +0100

    fix: typing to avoid Typescript complaint

--- pipe.ts
+++ pipe.ts
@@ -2,4 +2,4 @@
  * Eric Elliott's pipe function
  * @see https://medium.com/javascript-scene/composing-software-an-introduction-27b72500d6ea
  */
-export const pipe = (...fns) => (x = undefined) => fns.reduce((v, f) => f(v), x);
+export const pipe = <T = any>(...fns: any[]) => (x?: any) => fns.reduce<T>((v: any, f: any) => f(v), x);
