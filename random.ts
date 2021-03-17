a8d48d94c20a65f5c813d588ca9f4633e5eb1702
Leonardo Shinagawa <leonardo@ebussola.com>
2021-03-17 19:22:57 +0100

    added randString

--- random.ts
+++ random.ts
@@ -5,3 +5,11 @@ export function getOneRandomly<T>(arr: T[]): T {
     const randomId = Math.floor(Math.random() * arr.length);
     return arr[randomId];
 }
+
+/**
+ * Generates random strings
+ * Most of the time used as ID
+ */
+export function randString(): string {
+    return Math.random().toString(36).substr(2, 11);
+}
