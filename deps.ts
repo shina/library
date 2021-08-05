bf827067dfa98951fbddd373a2bbb61cf436ac12
Leonardo Shinagawa <leonardo@ebussola.com>
2021-08-05 08:33:26 +0200

    removed external dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,2 @@
 export { assert, assertEquals, assertThrows } from "https://deno.land/std@0.101.0/testing/asserts.ts";
 export * as asserts from "https://deno.land/std@0.101.0/testing/asserts.ts";
-export { dirname, fromFileUrl } from "https://deno.land/std@0.101.0/path/mod.ts";
--- src/path.ts
+++
@@ -1,5 +0,0 @@
-import { dirname, fromFileUrl } from '../deps.ts';
-
-export function dirFromFileUrl(fileUrl: string) {
-    return dirname(fromFileUrl(fileUrl));
-}
