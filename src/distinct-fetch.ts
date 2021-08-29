6d7c3c33fc312b6dadc59071c6ba3c7c106cee9c
Leonardo Shinagawa <leonardo@ebussola.com>
2021-08-29 22:54:47 +0200

    added `DistinctFetch` for auto cancel requests when it repeats

---
+++ src/distinct-fetch.ts
@@ -0,0 +1,40 @@
+/**
+ * DistinctFetch has a method `fetch` which is intended to be like the regular `fetch`
+ * but it never do another request while some other request is being made (by this particular instance)
+ *
+ * When the `fetch` is called while another request is in progress, it cancels that and start the
+ * new one, throwing an Exception to the cancelled request.
+ */
+import { executeOrIgnore } from "./type-checking.ts";
+
+export class DistinctFetch {
+
+    private abortController?: AbortController;
+
+    constructor(
+        private fetchFn: typeof fetch,
+        ignoreError = false
+    ) {
+        if (ignoreError) {
+            this.fetchFn = executeOrIgnore(
+                this.fetchFn,
+                (e: Error) => {
+                    console.log(e);
+                    return e.name === "AbortError";
+                }
+            ) as typeof fetch;
+        }
+    }
+
+    fetch: typeof fetch = (url: string | Request | URL, fetchOptions?: RequestInit) => {
+        this.abortController?.abort();
+        this.abortController = new AbortController();
+
+        fetchOptions = {
+            ...fetchOptions ?? {},
+            signal: this.abortController.signal
+        };
+        return this.fetchFn(url, fetchOptions);
+    }
+
+}
