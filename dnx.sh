123d691c6aaf5af0a6c05aef7d5c199a4107b9ba
Leonardo Shinagawa <leonardo@ebussola.com>
2021-09-05 12:40:38 +0200

    using Denox

--- dnx.sh
+++
@@ -1,3 +0,0 @@
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-deno run --allow-run tasks.ts $1 $SCRIPTPATH
--- tasks.ts
+++ tasks.ts
@@ -1,26 +1,6 @@
-const scriptPath = Deno.args[1];
-const tasks = {
-    install: `deno cache ${scriptPath}/deps.ts --lock=${scriptPath}/lock.json`,
-    update: `deno cache ${scriptPath}/deps.ts --lock=${scriptPath}/lock.json --lock-write`,
-}
+const cwd = Deno.cwd();
 
-function getDenoCmd(cmd: string): string[] {
-    return cmd.split(" ");
-}
-
-async function exec(cmd: string) {
-    const proc = await Deno.run({ cmd: getDenoCmd(cmd) }).status();
-
-    if (proc.success == false) {
-        Deno.exit(proc.code);
-    }
-
-    return proc;
-}
-
-const taskName = Deno.args[0] as keyof typeof tasks;
-if (taskName) {
-    exec(tasks[taskName]);
-} else {
-    console.log(tasks);
+export default {
+    install: `deno cache ${cwd}/deps.ts --lock=${cwd}/lock.json`,
+    update: `deno cache ${cwd}/deps.ts --lock=${cwd}/lock.json --lock-write`,
 }
