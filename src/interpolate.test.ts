0905c85f61e91b7e76e32e015a0f3dd5ac461b2a
Leonardo Shinagawa <leonardo@ebussola.com>
2021-11-17 20:41:06 +0100

    fix(interpolate): bugs

--- src/interpolate.test.ts
+++ src/interpolate.test.ts
@@ -10,4 +10,18 @@ describe("interpolate", (it) => {
   it("should work with space between the mustache", () => {
     assertEquals(interpolate("replace {{ foo }}", { foo: "me" }), "replace me");
   });
+
+  it("should replace all the occurrences in the string", () => {
+    assertEquals(
+      interpolate("replace {{ foo }} {{ foo }}", { foo: "me" }),
+      "replace me me",
+    );
+  });
+
+  it("should be able to use multiple values in the same obj", () => {
+    assertEquals(
+      interpolate("{{replace}} {{ foo }}", { replace: "hello", foo: "world" }),
+      "hello world",
+    );
+  });
 });
--- src/interpolate.ts
+++ src/interpolate.ts
@@ -1,9 +1,8 @@
 export function interpolate(text: string, vars: Record<string, string>) {
   return Object.entries(vars)
     .reduce((result: string, [key, value]) => {
-      result = result
-        .replace(`{{${key}}}`, value)
-        .replace(`{{ ${key} }}`, value);
-      return result;
+      return result
+        .replaceAll(`{{${key}}}`, value)
+        .replaceAll(`{{ ${key} }}`, value);
     }, text);
 }
