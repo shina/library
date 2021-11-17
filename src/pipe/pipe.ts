d9bbc1e0705396ed3e99d02d6f3ea09534f4dfa9
Leonardo Shinagawa <leonardo@ebussola.com>
2021-11-17 18:20:27 +0100

    fix(pipe): type

--- src/pipe/pipe.ts
+++ src/pipe/pipe.ts
@@ -4,8 +4,8 @@
  * Eric Elliott's pipe function
  * @see https://medium.com/javascript-scene/composing-software-an-introduction-27b72500d6ea
  */
-export const pipe = <T>(...fns: CallableFunction[]) =>
-  (x?: T) => fns.reduce((v: T | undefined, f: CallableFunction) => f(v), x);
+export const pipe = <U>(...fns: CallableFunction[]) =>
+  (x?: any) => fns.reduce<U>((v: any, f: CallableFunction) => f(v), x);
 
 export const pipeFrom = <U>(x?: any) =>
   (...fns: CallableFunction[]) =>
